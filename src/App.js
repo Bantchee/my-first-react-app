30907141e12e004b5fc39a241f75b58cbb56bfde
Jar'Zeno <jarzenojarrett@gmail.com>
2022-12-18 14:15:33 -0800

    Add MyComponent react component

--- src/App.js
+++ src/App.js
@@ -1,33 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
-import { Card } from './components/card.js'
+import React, { Component } from 'react';
+import { MyComponent } from './components/MyComponent.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p className="not">This is not the page you are looking for!</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <div>
-        <p>
-          Hi, I'm carol!
-        </p>
-      </div>
-      <Card></Card>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      count: 0,
+    }
+
+    this.countUp = this.countUp.bind(this);
+  }
+
+  countUp() {
+    this.setState({
+      count: this.state.count + 1,
+    });
+  }
+
+   render() {
+     return (
+       <div>
+         <MyComponent title="React" onButtonClicked={this.countUp} />
+         <p>{this.state.count}</p>
+       </div>
+     )
+   }
 }
 
 export default App;
